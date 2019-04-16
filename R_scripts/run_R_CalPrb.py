a792b2c95bbbb85f859e3c3406b73ad1fd2cde00
Genevieve <gjohnson6@luc.edu>
2019-04-16 01:30:26 +0000

    removed old files

--- R_scripts/run_R_CalPrb.py
+++
@@ -1,7 +0,0 @@
-import rpy2.robjects as robjects
-
-r_source = robjects.r["source"]
-z = r_source("CalPrb_commands.R")
-
-print("CalPrb finished running")
-print(z)
--- R_scripts/run_R_loocv.py
+++
@@ -1,7 +0,0 @@
-import rpy2.robjects as robjects
-
-r_source = robjects.r["source"]
-c = r_source("loocv_commands.R")
-
-print("loocv finished running")
-print(c)
